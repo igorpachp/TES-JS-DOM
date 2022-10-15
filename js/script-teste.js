63977868d3f550ad68391042f80c2399e15b23e1
igorpachp <igorpacheco9@gmail.com>
2022-10-15 20:12:18 -0300

    criado botao para criar circulos, cor escolhida automaticamente

--- js/script-teste.js
+++ js/script-teste.js
@@ -1,24 +1,59 @@
-//  page elements
-const mainBlock = createMain();
+// container divs
+const buttonsBlock = createButtonsBlock();
+const mainBlock = createMainBlock();
 
-const c_blue = createCircle("blue");
-const c_red = createCircle("red");
-const c_yellow = createCircle("yellow");
+// buttons
+const incButton = createIncButton();
 
-function createMain() {
-    let mainBlock = document.createElement("div");
-    mainBlock.classList.add("main");
+// variables
+let numberOfCircles = 0;
+
+function createMainBlock() {
+    const mainBlock = document.createElement("div");
+
+    mainBlock.id = "main";
     document.body.appendChild(mainBlock);
 
     return mainBlock;
 }
 
-function createCircle(color) {
+function createButtonsBlock() {
+    const buttonsDiv = document.createElement("div");
+
+    buttonsDiv.id = "buttons";
+    document.body.appendChild(buttonsDiv);
+
+    return buttonsDiv;
+}
+
+function createIncButton() {
+    const incButton = document.createElement("button");
+    incButton.innerHTML = "+1 circle";
+    incButton.onclick = () => {increaseCircleCount()};
+
+    buttonsBlock.appendChild(incButton);
+
+    return incButton;
+}
+
+function increaseCircleCount(){
+    createCircle();
+    numberOfCircles++;
+};
+
+function createCircle() {
     const newCircle = document.createElement("div");
- 
+
     newCircle.classList.add("circle");
-    newCircle.id = color;
     newCircle.onclick = () => {hide_circle(newCircle)};
+    if (numberOfCircles % 3 == 0) {
+        color = 'red';
+    } else if (numberOfCircles % 3 == 1) {
+        color = 'blue';
+    } else  {
+        color = 'yellow';
+    }
+    newCircle.id = color;
 
     mainBlock.appendChild(newCircle);
 
