dd8b4daadac1ffa1cb9dee6ec6dca44e0c7d8b3d
igorpachp <igorpacheco9@gmail.com>
2022-10-15 19:56:05 -0300

    adicionada função para esconder circulo ao clicar

--- js/script-teste.js
+++ js/script-teste.js
@@ -4,9 +4,16 @@ const c_red = createCircle("red");
 const c_yellow = createCircle("yellow");
 
 function createCircle(color) {
-    let newCircle = document.createElement("div");
-    newCircle.classList.add("circle", color);
+    const newCircle = document.createElement("div");
+    newCircle.classList.add("circle");
+    newCircle.id = color;
+    newCircle.onclick = () => {hide_circle(color)};
     document.body.appendChild(newCircle);
 
     return newCircle;
 }
+
+function hide_circle(color){
+    const target = document.getElementById(color);
+    target.style.display = "none";
+};
