f997b63687ac8245862c4ae37c9f6cf26fedde32
igorpachp <igorpacheco9@gmail.com>
2022-10-15 20:33:38 -0300

    adicionado botao para remover circulos

--- js/script-teste.js
+++ js/script-teste.js
@@ -5,11 +5,13 @@ const mainBlock = createMainBlock();
 // buttons
 const incButton = createIncButton();
 const resetButton = createResetButton();
+const decButton =  createDecButton();
 
 // variables
 let numberOfCircles = 0;
 let hiddenStack = []; // stack keeping hidden circles
 
+// creating container divs
 function createMainBlock() {
     const mainBlock = document.createElement("div");
 
@@ -28,6 +30,7 @@ function createButtonsBlock() {
     return buttonsDiv;
 }
 
+// creating buttons
 function createIncButton() {
     const incButton = document.createElement("button");
     incButton.innerHTML = "+1 circle";
@@ -48,11 +51,25 @@ function createResetButton() {
     return rButton;
 }
 
+function createDecButton() {
+    const decButton = document.createElement("button");
+    decButton.innerHTML = "-1 circle";
+    decButton.onclick = () => {decreaseCircleCount()};
+
+    buttonsBlock.appendChild(decButton);
+
+    return decButton;
+}
+
+// interactive functions
+// this function adds a circle to the total count
 function increaseCircleCount(){
     createCircle();
     numberOfCircles++;
 };
 
+// this function is responsible for 
+// actually creating the new circle
 function createCircle() {
     const newCircle = document.createElement("div");
 
@@ -72,14 +89,43 @@ function createCircle() {
     return newCircle;
 }
 
+// this function removes a circle from the screen
+// by changing it's display to 'none'
+// the element is not removed from DOM,
+// instead it is moved to a stack with
+// other hidden circles
 function hide_circle(element) {
     element.style.display = "none";
     hiddenStack.push(element);
 };
 
+// this function removes all circles from the 
+// stack of hidden elements, making them visible again
 function resetCirclesDisplay() {
     while (hiddenStack.length > 0) {
         element = hiddenStack.pop();
         element.style.display = 'block';
     }
 }
+
+// this function decreases the number of circles
+// and actually deletes it from DOM
+function decreaseCircleCount(){
+    element = null;
+
+    // if there are hidden circles,
+    // they will be deleted first
+    if (hiddenStack.length > 0) {
+        element = hiddenStack.pop();
+    } else {
+    	// elements are deleted first to last created
+        if (numberOfCircles > 0) {
+            element = mainBlock.firstChild;
+        }
+    }
+
+    if (element != null) {
+        mainBlock.removeChild(element);
+        numberOfCircles--;
+    }
+};
