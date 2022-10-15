7f61da4f61c470e2860b8ce917de0cf2970f7095
igorpachp <igorpacheco9@gmail.com>
2022-10-15 20:01:13 -0300

    removido o bug que deletava sempre o primeiro circulo, cores, bloco principal

--- js/script-teste.js
+++ js/script-teste.js
@@ -1,19 +1,30 @@
 //  page elements
+const mainBlock = createMain();
+
 const c_blue = createCircle("blue");
 const c_red = createCircle("red");
 const c_yellow = createCircle("yellow");
 
+function createMain() {
+    let mainBlock = document.createElement("div");
+    mainBlock.classList.add("main");
+    document.body.appendChild(mainBlock);
+
+    return mainBlock;
+}
+
 function createCircle(color) {
     const newCircle = document.createElement("div");
+ 
     newCircle.classList.add("circle");
     newCircle.id = color;
-    newCircle.onclick = () => {hide_circle(color)};
-    document.body.appendChild(newCircle);
+    newCircle.onclick = () => {hide_circle(newCircle)};
+
+    mainBlock.appendChild(newCircle);
 
     return newCircle;
 }
 
-function hide_circle(color){
-    const target = document.getElementById(color);
-    target.style.display = "none";
+function hide_circle(element){
+    element.style.display = "none";
 };
